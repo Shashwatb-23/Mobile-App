3e1e26876b35dca9164238d5379159cffb42c6a2
Shashwat Bhardwaj <shashwatbhardwaj3@gmail.com>
2024-04-06 16:11:17 +0530

    making a mobile app and using firebase Database to store the things

---
+++ script.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    getDatabaseURL:"https://playground-fb245-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    push(shoppingListInDB, inputValue)
+    console.log(inputValue)
+})
